cea2a9e30a3805e7362b5f07e57ee8fdc18159f1
Sebastian <sbusek@email.cz>
2019-09-02 22:18:17 +0200

    bugfix(callout): fixed callout background

--- src/utils/toasts.ts
+++ src/utils/toasts.ts
@@ -1,3 +1,4 @@
+import { type } from '../components/callout/callout.type';
 
 let toastController: HTMLXuiToastControllerElement
 
@@ -5,17 +6,22 @@ export const init = async ({ toastCtrl }) => {
   toastController = await toastCtrl.componentOnReady();
 };
 
-export const showToast = async ({ text }: { text: string }) => {
+export const message = async ({ text, type }: { 
+  text: string,
+  type: type | null,
+}) => {
   while (!toastController) {
     await wait(10)
   }
 
-  const toast = await toastController.message({
-    text
-  })
+  const toast = await toastController.message({ text, type})
 
   return toast;
 };
+
+export const info = async (text: string) => message({ text, type: 'info' })
+export const success = async (text: string) => message({ text, type: 'success' })
+export const error = async (text: string) => message({ text, type: 'danger' })
 
 async function wait(timeout: number) {
   return new Promise((res) => setTimeout(() => res(), timeout))
